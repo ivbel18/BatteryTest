0c00b1a90bb64ec462df51bcd036c46d6f995c70
ivbel18 <ivbel18@student.sdu.dk>
2018-10-19 14:01:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ivobelyasin/batterytest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.ivobelyasin.batterytest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView batteryLevel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batteryLevel = (TextView) findViewById(R.id.batteryLevel);
+}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(batteryChangeReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(batteryChangeReceiver);
+    }
+
+    private BroadcastReceiver batteryChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent i) {
+            int level = i.getIntExtra("level", 0);
+            batteryLevel.setText(String.valueOf(level));
+        }
+
+    };
+}
